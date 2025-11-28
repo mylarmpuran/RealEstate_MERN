69d53b28474d53376089bb8ca5123b5dd83ec8f6
puran kumar <mylarmpuran@gmail.com>
2025-11-28 23:08:00 +0530

    test api

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const test = (req, res) => {
+    res.json({
+        message:'Hello World'
+    })
+}
+
+
+export default test;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to MongoDB!');
@@ -18,3 +20,5 @@ const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}!!!!`);
 })
+
+app.use('/api/user', userRouter)
