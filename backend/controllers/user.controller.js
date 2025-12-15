832c080f89503789ec1fd8b7a187267f0268c1da
puran kumar <mylarmpuran@gmail.com>
2025-12-15 21:29:33 +0530

    create user update api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,7 @@
+import errorHandler from "../utils/error"
+import bcryptjs from "bcryptjs";
+
+
 const test = (req, res) => {
     console.log('hello world')
     res.json({
@@ -5,5 +9,28 @@ const test = (req, res) => {
     })
 }
 
+export const updatedUser = async(req, res, next) => {
+    if(req.user.id != req.params.id) return next(errorHandler(401, 'You can only update your own account!'))
 
+        try {
+            if(req.body.password){
+                req.body.password = bcryptjs.hashSync(req.body.password, 10)
+            }
+
+            const updateUser = await User.findByIdAndUpdate(req.params.id,{
+                $set:{
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: req.body.avatar,
+                }
+            }, {new:true})
+
+            const { password, ...rest} = updatedUser._doc
+
+            res.status(200).json(rest);
+        } catch (error) {
+            next(error)
+        }
+}
 export default test;
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from "cookie-parser";
 import cors from 'cors'
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 app.use(express.json());
 app.use(cors({credentials: true}))
+app.use(cookieParser())
 
 const PORT = 5000;
 
