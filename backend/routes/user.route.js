8f1ed5fc76b619806fb09e595df82bab064759a4
puran kumar <mylarmpuran@gmail.com>
2025-12-17 15:59:41 +0530

    create get user listing

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import test, { deleteUser, updatedUser } from "../controllers/user.controller.js";
+import test, { deleteUser, getUserListings, updatedUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verfyUser.js";
 
 const router = express.Router();
@@ -8,5 +8,7 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id',verifyToken,updatedUser)
 router.delete('/delete/:id',verifyToken,deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+
 
 export default router;
