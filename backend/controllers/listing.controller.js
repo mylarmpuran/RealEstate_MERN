3728540f45c44ea7b59527a8c8727a58bfd18ab3
puran kumar <mylarmpuran@gmail.com>
2025-12-19 08:38:42 +0530

    create search API route

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -81,8 +81,39 @@ export const getListings = async (req, res, next) => {
       furnished = {$in: [false, true]};
     }
 
+    let parking = req.query.parking
+
+    if (parking == undefined || parking === 'false'){
+      parking = { $in: [false, true]};
+    }
+
+    let type = req.query.type;
+
+    if(type === undefined || type === 'all'){
+      type = { $in: ['sale', 'rent']};
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.quwery.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i'},
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+    .sort({ [sort]: order})
+    .limit(limit)
+    .skip(startIndex);
+
+    return res.status(200).json(listings);
+
     
   } catch (error) {
-    
+    next(error)
   }
 }
